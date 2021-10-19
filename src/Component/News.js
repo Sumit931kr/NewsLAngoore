ffd2b600cea3980fc1de568cdbfe3a5abc992be3
Sumit <sumitkumar280601@gmail.com>
2021-10-19 10:18:17 +0530

    Tutorial 23 is complete

---
+++ src/Component/News.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Newsitem from './Newsitem'
+
+export class news extends Component {
+    render() {
+        return (
+            <div>
+               <Newsitem/>
+               <Newsitem/>
+               <Newsitem/>
+               <Newsitem/>
+               <Newsitem/>
+               <Newsitem/>
+               <Newsitem/>
+
+            </div>
+        )
+    }
+}
+
+export default news
---
+++ src/Component/Newsitem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Newsitems extends Component {
+    render() {
+        return (
+            <div>
+                FOlks 2
+            </div>
+        )
+    }
+} 
+
+export default Newsitems
