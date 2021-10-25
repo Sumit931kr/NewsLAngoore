10f7ab4bf39ed84742951ae585aa6634413664a6
Sumit <sumitkumar280601@gmail.com>
2021-10-25 19:25:45 +0530

    Tutorial 27 completed

--- src/Component/Newsitem.js
+++ src/Component/Newsitem.js
@@ -8,9 +8,9 @@ export class Newsitems extends Component {
         return (
             <div>
                 <div className="card my-3 container" style={{width: '18rem'}}>
-  <img src={imageUrl} className="card-img-top" alt="Hope" />
+  <img src={!imageUrl?"https://images.moneycontrol.com/static-mcnews/2021/10/stocks_sensex_nifty_stockmarkets-2-770x433.jpg":imageUrl} className="card-img-top" alt="Hope" />
   <div className="card-body">
-    <h5 className="card-title">{title}...</h5>
+    <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}...</p>
     <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">Read More About it</a>
   </div>
